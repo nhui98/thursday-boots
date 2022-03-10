7056dab8444c6154065bdf6c83c4f7dd350258ab
Nathan <hui.n1998@gmail.com>
2022-03-10 23:39:34 +0000

    split feature into lg screen and sm screen components, completed lg screen component and styling

---
+++ client/src/pages/home/featured-collection/featured-sm/featured-sm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Featuredsm = () => {
+    return (
+        <section className="featuredsm">
+            hello
+        </section>
+    )
+}
+
+export default Featuredsm
