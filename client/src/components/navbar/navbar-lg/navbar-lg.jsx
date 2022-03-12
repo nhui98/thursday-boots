d75a0bbc4bc202bbbea6914e2ebc2d658a7a4c5b
Nathan <hui.n1998@gmail.com>
2022-03-12 13:59:02 +0000

    first iteration of homepage completed

--- client/src/components/navbar/navbar-lg/navbar-lg.jsx
+++ client/src/components/navbar/navbar-lg/navbar-lg.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { BiSearch, BiShoppingBag } from "react-icons/bi"
 import { AiOutlineDown } from "react-icons/ai"
 
