7b3b8f817e35611866ee12db2f3a7d051218fb67
Nathan <hui.n1998@gmail.com>
2022-03-12 11:47:16 +0000

    navbar large, added dropdown categories

--- client/src/components/navbar/navbar-lg/navbar-lg.jsx
+++ client/src/components/navbar/navbar-lg/navbar-lg.jsx
@@ -6,14 +6,72 @@ const NavbarLg = () => {
     return (
         <nav className="navbar">
             <ul className="navbar__left">
-                <li className="navbar__left-link">
+                <li className="navbar__left-link men">
                     <a href="/">men <AiOutlineDown /></a>
                 </li>
-                <li className="navbar__left-link">
+                <div className="dropdown">
+                    <ul className="dropdown-links">
+                        <li className="dropdown-link"><a href="/">boots</a></li>
+                        <li className="dropdown-link"><a href="/">all styles</a></li>
+                        <li className="dropdown-link"><a href="/">lace-ups</a></li>
+                        <li className="dropdown-link"><a href="/">chelsea</a></li>
+                        <li className="dropdown-link"><a href="/">chukka</a></li>
+                        <li className="dropdown-link"><a href="/">rugged & resilient</a></li>
+                    </ul>
+                    <ul className="dropdown-links">
+                        <li className="dropdown-link"><a href="/">sneakers</a></li>
+                        <li className="dropdown-link"><a href="/">all styles</a></li>
+                        <li className="dropdown-link"><a href="/">high tops</a></li>
+                        <li className="dropdown-link"><a href="/">low tops</a></li>
+                    </ul>
+                    <ul className="dropdown-links">
+                        <li className="dropdown-link"><a href="/">shoes</a></li>
+                        <li className="dropdown-link"><a href="/">dress shoes</a></li>
+                        <li className="dropdown-link"><a href="/">loafers</a></li>
+                    </ul>
+                    <ul className="dropdown-links">
+                        <li className="dropdown-link"><a href="/">accessories & apparel</a></li>
+                        <li className="dropdown-link"><a href="/">shoe care & laces</a></li>
+                        <li className="dropdown-link"><a href="/">socks</a></li>
+                        <li className="dropdown-link"><a href="/">belts</a></li>
+                        <li className="dropdown-link"><a href="/">jackets</a></li>
+                        <li className="dropdown-link"><a href="/">wallets</a></li>
+                    </ul>
+                </div>
+                <li className="navbar__left-link women">
                 <a href="/">women <AiOutlineDown /></a>
                 </li>
+                <div className="dropdown">
+                    <ul className="dropdown-links">
+                        <li className="dropdown-link"><a href="/">boots</a></li>
+                        <li className="dropdown-link"><a href="/">all styles</a></li>
+                        <li className="dropdown-link"><a href="/">lace-ups</a></li>
+                        <li className="dropdown-link"><a href="/">chelsea</a></li>
+                        <li className="dropdown-link"><a href="/">chukka</a></li>
+                        <li className="dropdown-link"><a href="/">rugged & resilient</a></li>
+                    </ul>
+                    <ul className="dropdown-links">
+                        <li className="dropdown-link"><a href="/">sneakers</a></li>
+                        <li className="dropdown-link"><a href="/">all styles</a></li>
+                        <li className="dropdown-link"><a href="/">high tops</a></li>
+                        <li className="dropdown-link"><a href="/">low tops</a></li>
+                    </ul>
+                    <ul className="dropdown-links">
+                        <li className="dropdown-link"><a href="/">shoes</a></li>
+                        <li className="dropdown-link"><a href="/">dress shoes</a></li>
+                        <li className="dropdown-link"><a href="/">loafers</a></li>
+                    </ul>
+                    <ul className="dropdown-links">
+                        <li className="dropdown-link"><a href="/">accessories & apparel</a></li>
+                        <li className="dropdown-link"><a href="/">shoe care & laces</a></li>
+                        <li className="dropdown-link"><a href="/">socks</a></li>
+                        <li className="dropdown-link"><a href="/">belts</a></li>
+                        <li className="dropdown-link"><a href="/">jackets</a></li>
+                        <li className="dropdown-link"><a href="/">wallets</a></li>
+                    </ul>
+                </div>
                 <li className="navbar__left-link">
-                    <a href="/">about <AiOutlineDown /></a>
+                    <a href="/">about</a>
                 </li>
             </ul>
             <div className="navbar__center">
