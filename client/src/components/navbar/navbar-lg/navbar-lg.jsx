54bbbd989b579151eeaca4dd71c6ee10d68dc64c
Nathan <hui.n1998@gmail.com>
2022-03-12 14:20:30 +0000

    made navbar sm and large component render method more lean

--- client/src/components/navbar/navbar-lg/navbar-lg.jsx
+++ client/src/components/navbar/navbar-lg/navbar-lg.jsx
@@ -1,6 +1,40 @@
-import React, { useEffect, useRef, useState } from "react"
+import React from "react"
 import { BiSearch, BiShoppingBag } from "react-icons/bi"
 import { AiOutlineDown } from "react-icons/ai"
+
+const DropDown = () => {
+    return (
+        <div className="dropdown">
+            <ul className="dropdown-links">
+                <li className="dropdown-link"><a href="/">boots</a></li>
+                <li className="dropdown-link"><a href="/">all styles</a></li>
+                <li className="dropdown-link"><a href="/">lace-ups</a></li>
+                <li className="dropdown-link"><a href="/">chelsea</a></li>
+                <li className="dropdown-link"><a href="/">chukka</a></li>
+                <li className="dropdown-link"><a href="/">rugged & resilient</a></li>
+            </ul>
+            <ul className="dropdown-links">
+                <li className="dropdown-link"><a href="/">sneakers</a></li>
+                <li className="dropdown-link"><a href="/">all styles</a></li>
+                <li className="dropdown-link"><a href="/">high tops</a></li>
+                <li className="dropdown-link"><a href="/">low tops</a></li>
+            </ul>
+            <ul className="dropdown-links">
+                <li className="dropdown-link"><a href="/">shoes</a></li>
+                <li className="dropdown-link"><a href="/">dress shoes</a></li>
+                <li className="dropdown-link"><a href="/">loafers</a></li>
+            </ul>
+            <ul className="dropdown-links">
+                <li className="dropdown-link"><a href="/">accessories & apparel</a></li>
+                <li className="dropdown-link"><a href="/">shoe care & laces</a></li>
+                <li className="dropdown-link"><a href="/">socks</a></li>
+                <li className="dropdown-link"><a href="/">belts</a></li>
+                <li className="dropdown-link"><a href="/">jackets</a></li>
+                <li className="dropdown-link"><a href="/">wallets</a></li>
+            </ul>
+        </div>
+    )
+}
 
 const NavbarLg = () => {
     return (
@@ -9,67 +43,11 @@ const NavbarLg = () => {
                 <li className="navbar__left-link men">
                     <a href="/">men <AiOutlineDown /></a>
                 </li>
-                <div className="dropdown">
-                    <ul className="dropdown-links">
-                        <li className="dropdown-link"><a href="/">boots</a></li>
-                        <li className="dropdown-link"><a href="/">all styles</a></li>
-                        <li className="dropdown-link"><a href="/">lace-ups</a></li>
-                        <li className="dropdown-link"><a href="/">chelsea</a></li>
-                        <li className="dropdown-link"><a href="/">chukka</a></li>
-                        <li className="dropdown-link"><a href="/">rugged & resilient</a></li>
-                    </ul>
-                    <ul className="dropdown-links">
-                        <li className="dropdown-link"><a href="/">sneakers</a></li>
-                        <li className="dropdown-link"><a href="/">all styles</a></li>
-                        <li className="dropdown-link"><a href="/">high tops</a></li>
-                        <li className="dropdown-link"><a href="/">low tops</a></li>
-                    </ul>
-                    <ul className="dropdown-links">
-                        <li className="dropdown-link"><a href="/">shoes</a></li>
-                        <li className="dropdown-link"><a href="/">dress shoes</a></li>
-                        <li className="dropdown-link"><a href="/">loafers</a></li>
-                    </ul>
-                    <ul className="dropdown-links">
-                        <li className="dropdown-link"><a href="/">accessories & apparel</a></li>
-                        <li className="dropdown-link"><a href="/">shoe care & laces</a></li>
-                        <li className="dropdown-link"><a href="/">socks</a></li>
-                        <li className="dropdown-link"><a href="/">belts</a></li>
-                        <li className="dropdown-link"><a href="/">jackets</a></li>
-                        <li className="dropdown-link"><a href="/">wallets</a></li>
-                    </ul>
-                </div>
+                <DropDown />
                 <li className="navbar__left-link women">
                 <a href="/">women <AiOutlineDown /></a>
                 </li>
-                <div className="dropdown">
-                    <ul className="dropdown-links">
-                        <li className="dropdown-link"><a href="/">boots</a></li>
-                        <li className="dropdown-link"><a href="/">all styles</a></li>
-                        <li className="dropdown-link"><a href="/">lace-ups</a></li>
-                        <li className="dropdown-link"><a href="/">chelsea</a></li>
-                        <li className="dropdown-link"><a href="/">chukka</a></li>
-                        <li className="dropdown-link"><a href="/">rugged & resilient</a></li>
-                    </ul>
-                    <ul className="dropdown-links">
-                        <li className="dropdown-link"><a href="/">sneakers</a></li>
-                        <li className="dropdown-link"><a href="/">all styles</a></li>
-                        <li className="dropdown-link"><a href="/">high tops</a></li>
-                        <li className="dropdown-link"><a href="/">low tops</a></li>
-                    </ul>
-                    <ul className="dropdown-links">
-                        <li className="dropdown-link"><a href="/">shoes</a></li>
-                        <li className="dropdown-link"><a href="/">dress shoes</a></li>
-                        <li className="dropdown-link"><a href="/">loafers</a></li>
-                    </ul>
-                    <ul className="dropdown-links">
-                        <li className="dropdown-link"><a href="/">accessories & apparel</a></li>
-                        <li className="dropdown-link"><a href="/">shoe care & laces</a></li>
-                        <li className="dropdown-link"><a href="/">socks</a></li>
-                        <li className="dropdown-link"><a href="/">belts</a></li>
-                        <li className="dropdown-link"><a href="/">jackets</a></li>
-                        <li className="dropdown-link"><a href="/">wallets</a></li>
-                    </ul>
-                </div>
+                <DropDown />
                 <li className="navbar__left-link">
                     <a href="/">about</a>
                 </li>
--- client/src/components/navbar/navbar-sm/navbar-sm.jsx
+++ client/src/components/navbar/navbar-sm/navbar-sm.jsx
@@ -2,14 +2,12 @@ import React, { useState } from "react"
 import { BiSearch, BiShoppingBag } from "react-icons/bi"
 import { MdOutlineAccountCircle } from "react-icons/md"
 import { GiHamburgerMenu } from "react-icons/gi"
-import { AiOutlineClose, AiOutlinePlus, AiOutlineMinus} from "react-icons/ai"
+import { AiOutlineClose, AiOutlinePlus, AiOutlineMinus } from "react-icons/ai"
 
-const NavbarSm = () => {
-    const [flyoutToggle, setFlyoutToggle] = useState(false)
+const Flyout = ({ flyoutToggle, setFlyoutToggle }) => {
     const [dropDown, setDropDown] = useState({
         men: false,
         women: false,
-
     })
 
     const dropdownHandler = e => {
@@ -22,6 +20,93 @@ const NavbarSm = () => {
             return {...prevState, women: !prevState.women}
         })
     }
+
+    return (
+        <aside className={`navflyout${flyoutToggle ? " active" : ""}`}>
+            <header className="navflyout__header">
+                <div className="navflyout__header-search">
+                    <BiSearch />
+                </div>
+                <div className="navflyout__header-close" onClick={() => setFlyoutToggle(false)}>
+                    <AiOutlineClose />
+                </div>
+            </header>
+
+            <section className="navflyout__body">
+                <div className="accordion">
+                    <div className={`levelone-header${dropDown.men ? " active" : ""}`} data-men onClick={e => dropdownHandler(e)}>
+                        <h2 className="levelone-title">
+                            men
+                        </h2>
+                        <div className="toggle-icons">
+                            <div className={`plus${dropDown.men ? " active" : ""}`}>
+                                <AiOutlinePlus />
+                            </div>
+                            <div className={`minus${dropDown.men ? "" : " active"}`}>
+                                <AiOutlineMinus />
+                            </div>
+                        </div>
+                    </div>
+                    <ul className="levelone-dropdown">
+                        <li className="dropdown-item">
+                            <a href="/">boots</a>
+                        </li>
+                        <li className="dropdown-item">
+                            <a href="/">sneakers</a>
+                        </li>
+                        <li className="dropdown-item">
+                            <a href="/">shoes</a>
+                        </li>
+                        <li className="dropdown-item">
+                            <a href="/">accessories & apparel</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="accordion">
+                    <div
+                        className={`levelone-header${dropDown.women ? " active" : ""}`} data-women
+                        onClick={e => dropdownHandler(e)}>
+                        <h2 className="levelone-title">
+                            women
+                        </h2>
+                        <div className="toggle-icons">
+                            <div className={`plus${dropDown.men ? " active" : ""}`}>
+                                <AiOutlinePlus />
+                            </div>
+                            <div className={`minus${dropDown.men ? "" : " active"}`}>
+                                <AiOutlineMinus />
+                            </div>
+                        </div>
+                    </div>
+                    <ul className="levelone-dropdown">
+                        <li className="dropdown-item">
+                            <a href="/">boots</a>
+                        </li>
+                        <li className="dropdown-item">
+                            <a href="/">sneakers</a>
+                        </li>
+                        <li className="dropdown-item">
+                            <a href="/">shoes</a>
+                        </li>
+                        <li className="dropdown-item">
+                            <a href="/">accessories & apparel</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="divide" />
+                <ul className="navflyout__body-links">
+                    <li className="navflyout__body-link"><a href="/">about</a></li>
+                    <li className="navflyout__body-link"><a href="/">help</a></li>
+                    <li className="navflyout__body-link"><a href="/">account</a></li>
+                    <li className="navflyout__body-link"><a href="/">gift cards</a></li>
+                </ul>
+            </section>
+        </aside>
+    )
+}
+
+const NavbarSm = () => {
+    const [flyoutToggle, setFlyoutToggle] = useState(false)
 
     return (
         <>
@@ -43,86 +128,7 @@ const NavbarSm = () => {
             </nav>
 
             {/* Flyout */}
-            <aside className={`navflyout${flyoutToggle ? " active" : ""}`}>
-                <header className="navflyout__header">
-                    <div className="navflyout__header-search">
-                        <BiSearch />
-                    </div>
-                    <div className="navflyout__header-close" onClick={() => setFlyoutToggle(false)}>
-                        <AiOutlineClose />
-                    </div>
-                </header>
-
-                <section className="navflyout__body">
-                    <div className="accordion">
-                        <div className={`levelone-header${dropDown.men ? " active" : ""}`} data-men onClick={e => dropdownHandler(e)}>
-                            <h2 className="levelone-title">
-                                men
-                            </h2>
-                            <div className="toggle-icons">
-                                <div className={`plus${dropDown.men ? " active" : ""}`}>
-                                    <AiOutlinePlus />
-                                </div>
-                                <div className={`minus${dropDown.men ? "" : " active"}`}>
-                                    <AiOutlineMinus />
-                                </div>
-                            </div>
-                        </div>
-                        <ul className="levelone-dropdown">
-                            <li className="dropdown-item">
-                                <a href="/">boots</a>
-                            </li>
-                            <li className="dropdown-item">
-                                <a href="/">sneakers</a>
-                            </li>
-                            <li className="dropdown-item">
-                                <a href="/">shoes</a>
-                            </li>
-                            <li className="dropdown-item">
-                                <a href="/">accessories & apparel</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="accordion">
-                        <div
-                            className={`levelone-header${dropDown.women ? " active" : ""}`} data-women
-                            onClick={e => dropdownHandler(e)}>
-                            <h2 className="levelone-title">
-                                women
-                            </h2>
-                            <div className="toggle-icons">
-                                <div className={`plus${dropDown.men ? " active" : ""}`}>
-                                    <AiOutlinePlus />
-                                </div>
-                                <div className={`minus${dropDown.men ? "" : " active"}`}>
-                                    <AiOutlineMinus />
-                                </div>
-                            </div>
-                        </div>
-                        <ul className="levelone-dropdown">
-                            <li className="dropdown-item">
-                                <a href="/">boots</a>
-                            </li>
-                            <li className="dropdown-item">
-                                <a href="/">sneakers</a>
-                            </li>
-                            <li className="dropdown-item">
-                                <a href="/">shoes</a>
-                            </li>
-                            <li className="dropdown-item">
-                                <a href="/">accessories & apparel</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="divide" />
-                    <ul className="navflyout__body-links">
-                        <li className="navflyout__body-link"><a href="/">about</a></li>
-                        <li className="navflyout__body-link"><a href="/">help</a></li>
-                        <li className="navflyout__body-link"><a href="/">account</a></li>
-                        <li className="navflyout__body-link"><a href="/">gift cards</a></li>
-                    </ul>
-                </section>
-            </aside>
+            <Flyout flyoutToggle={flyoutToggle} setFlyoutToggle={setFlyoutToggle} />
 
             {/* Overlay */}
             <div className={`overlay${flyoutToggle ? " active" : ""}`} onClick={() => setFlyoutToggle(false)} />
