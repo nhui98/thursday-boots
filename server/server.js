bd94c7d3b5506e69fa7c95cdfd7f84657e892ef3
Nathan <hui.n1998@gmail.com>
2022-03-24 23:11:25 +0000

    added cors

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const cors = require("cors")
 
 const { orderRouter } = require("./routers/orders.js")
 const { paymentRouter } = require("./routers/payments.js")
@@ -11,7 +12,8 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
 app.use("/api/products", productsRouter)
 app.use("/api/users", userRouter)
