4995f4f9b7dc060a0d6d79a22ca60f7cdf6b358a
Nathan <hui.n1998@gmail.com>
2022-03-30 12:16:51 +0100

    lazy and suspense

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { lazy, Suspense } from "react"
 import { Routes, Route }  from "react-router-dom"
 
 import "./App.scss"
@@ -6,15 +6,16 @@ import "./App.scss"
 import Navbar from "./components/navbar/navbar"
 import Footer from "./components/footer/footer"
 import Home from "./pages/home/home"
-import ProductDetails from "./pages/product-details/product-details"
-import ProductListing from "./pages/product-listing/product-listing"
-import Delivery from "./pages/checkout-delivery/delivery"
-import Signin from "./pages/account-signin/signin"
-import Signout from "./pages/account-signout/signout"
-import Payment from "./pages/checkout-payment/payment"
-import NotFound from "./pages/404/404"
-import Confirmation from "./pages/checkout-confirmation/confirmation"
-import Register from "./pages/account-register/register"
+import Loading from "./components/loading/loading"
+const Signin = lazy(() => import("./pages/account-signin/signin"))
+const Signout = lazy(() => import("./pages/account-signout/signout"))
+const Payment = lazy(() => import("./pages/checkout-payment/payment"))
+const NotFound = lazy(() => import("./pages/404/404"))
+const Confirmation = lazy(() => import("./pages/checkout-confirmation/confirmation"))
+const Register = lazy(() => import("./pages/account-register/register"))
+const Delivery = lazy(() => import("./pages/checkout-delivery/delivery"))
+const ProductListing = lazy(() => import("./pages/product-listing/product-listing"))
+const ProductDetails = lazy(() => import("./pages/product-details/product-details"))
 
 const PageWithFooter = props => {
     return (
@@ -28,28 +29,30 @@ const PageWithFooter = props => {
 const App = () => {
     return (
         <div className="thursday">
-            <Routes>
-                <Route path="/" element={<Navbar variant />} >
-                    <Route index element={<PageWithFooter><Home /></PageWithFooter>} />
-                </Route>
-                <Route path="/product-listing/:category/:type" element={<Navbar />} >
-                    <Route index element={<PageWithFooter><ProductListing /></PageWithFooter>} />
-                </Route>
-                <Route path="/product-details/:id" element={<Navbar />} >
-                    <Route index element={<PageWithFooter><ProductDetails /></PageWithFooter>} />
-                </Route>
-                <Route path="/signin" element={<Navbar />} >
-                    <Route index element={<Signin />} />
-                </Route>
-                <Route path="/register" element={<Navbar />} >
-                    <Route index element={<Register />} />
-                </Route>
-                <Route path="/signout" element={<Signout />} />
-                <Route path="/checkout/delivery" element={<Delivery />} />
-                <Route path="/checkout/payment" element={<Payment />} />
-                <Route path="/checkout/confirmation" element={<Confirmation /> }/>
-                <Route path="*" element={<NotFound />} />
-            </Routes>
+            <Suspense fallback={<Loading />}>
+                <Routes>
+                    <Route path="/" element={<Navbar variant />} >
+                        <Route index element={<PageWithFooter><Home /></PageWithFooter>} />
+                    </Route>
+                    <Route path="/product-listing/:category/:type" element={<Navbar />} >
+                        <Route index element={<PageWithFooter><ProductListing /></PageWithFooter>} />
+                    </Route>
+                    <Route path="/product-details/:id" element={<Navbar />} >
+                        <Route index element={<PageWithFooter><ProductDetails /></PageWithFooter>} />
+                    </Route>
+                    <Route path="/signin" element={<Navbar />} >
+                        <Route index element={<Signin />} />
+                    </Route>
+                    <Route path="/register" element={<Navbar />} >
+                        <Route index element={<Register />} />
+                    </Route>
+                    <Route path="/signout" element={<Signout />} />
+                    <Route path="/checkout/delivery" element={<Delivery />} />
+                    <Route path="/checkout/payment" element={<Payment />} />
+                    <Route path="/checkout/confirmation" element={<Confirmation /> }/>
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Suspense>         
         </div>
     )
 }
